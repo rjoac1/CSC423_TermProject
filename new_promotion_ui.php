e7f08d95c42fb3742ce1f648a8b21cad3174fde1
otijiko <otijiko@gmail.com>
2015-10-25 14:14:19 -0400

    Starting promotion ui done

--- new_promotion_ui.php
+++ new_promotion_ui.php
@@ -23,22 +23,22 @@ function ui_show_new_promotion_form()
 
     echo '<tr>';  //
     echo '<TD><SPAN ALIGN=RIGHT>Promotion Name:</SPAN></TD>';
-    echo '<TD><INPUT ID="promotionName" TYPE="text" SIZE=50/></TD>';
+    echo '<TD><INPUT ID="promotionName" NAME="promotionName" TYPE="text" SIZE=50/></TD>';
     echo '</tr>';
 
     echo '<tr>';  //
     echo '<TD><SPAN ALIGN=RIGHT>Promotion Description:</SPAN></TD>';
-    echo '<TD><INPUT ID="promotionDescription" TYPE="text" SIZE=50/></TD>';
+    echo '<TD><INPUT ID="promotionDescription" NAME="promotionDescription" TYPE="text" SIZE=50/></TD>';
     echo '</tr>';
 
     echo '<tr>';  //
     echo '<TD><SPAN ALIGN=RIGHT>Amount Off:</SPAN></TD>';
-    echo '<TD><INPUT ID="promotionValue" TYPE="text" SIZE=50/></TD>';
+    echo '<TD><INPUT ID="promotionValue" NAME="promotionValue" TYPE="text" SIZE=50/></TD>';
     echo '</tr>';
 
     echo '<tr>';  //
     echo '<TD><SPAN ALIGN=RIGHT>Promotion Type:</SPAN></TD>';
-    echo '<TD><select INPUT ID="promotionType">
+    echo '<TD><select INPUT ID="promotionType" NAME="promotionType">
               <option value="Dollar">Dollar</option>
               <option value="Percent">Percent</option>
               </select>
